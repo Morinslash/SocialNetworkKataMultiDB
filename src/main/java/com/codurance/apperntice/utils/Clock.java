875ec2be89bdc7ab56396f209627fc6c7c1c1824
Morinslash <sebastian.elf.kulinski@gmail.com>
2020-04-11 16:34:19 +0200

    green on clock

--- src/main/java/com/codurance/apperntice/utils/Clock.java
+++ src/main/java/com/codurance/apperntice/utils/Clock.java
@@ -1,7 +1,16 @@
 package com.codurance.apperntice.utils;
 
+import java.util.Date;
+
 public class Clock {
+
+    private final Date date;
+
+    public Clock() {
+        date = new Date();
+    }
+
     public long now() {
-        throw new UnsupportedOperationException("implement me!");
+        return date.getTime();
     }
 }
