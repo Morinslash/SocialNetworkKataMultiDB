2bb173a5cdc7dd25382d52f459d8b7a993b40135
Morinslash <sebastian.elf.kulinski@gmail.com>
2020-04-13 17:00:35 +0200

    small refator in command factory

--- src/main/java/com/codurance/apperntice/commands/CommandFactory.java
+++ src/main/java/com/codurance/apperntice/commands/CommandFactory.java
@@ -5,6 +5,9 @@ import com.codurance.apperntice.utils.Parser;
 import com.codurance.apperntice.service.UserService;
 
 public class CommandFactory {
+    public static final String POST_COMMAND = "->";
+    public static final String FOLLOW_COMMAND = "follows";
+    public static final String WALL_COMMAND = "wall";
     private Parser parser;
     private UserService userService;
 
@@ -16,17 +19,17 @@ public class CommandFactory {
     public Command getCommand(String userInput) {
         String[] parsedInput = parser.parseInput(userInput);
         User user = new User(parsedInput[0]);
-
+//TODO think about other solution for parsedInput
         if(parsedInput[1] == null){
             return new ReadCommand(user, userService);
         }
-        if(parsedInput[1].equals("->")){
+        if(parsedInput[1].equals(POST_COMMAND)){
             return new PostCommand(user,parsedInput[2], userService);
         }
-        if(parsedInput[1].equals("follows")){
+        if(parsedInput[1].equals(FOLLOW_COMMAND)){
             return new FollowCommand(user,parsedInput[2], userService);
         }
-        if(parsedInput[1].equals("wall")){
+        if(parsedInput[1].equals(WALL_COMMAND)){
             return new WallCommand(user, userService);
         }
         throw new UnsupportedOperationException("Implement me!");
